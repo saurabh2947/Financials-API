a94760b11c4382af16b987bb145759fa26054062
saurabh2947 <saurabh.kumar@vultech.co.in>
2020-05-03 02:00:35 +0530

    Implemented rudimentary class system

---
+++ Scraper project/Financials.py
@@ -0,0 +1,48 @@
+import xbrl, re
+import pandas as pd
+
+class financials:
+    def __init__(self,sc):
+        self.scripcode=sc
+
+    def bse_xmlparser(self):
+    
+        #initializaing dataframe
+        df=pd.DataFrame()
+
+        #parsing the XML file
+        print('Initializing...')
+        path="XML Files\\"
+        extension='.xml'
+        filename=path+str(self.scripcode)+extension
+        doc=str(xbrl.XBRLParser.parse(open(filename)))
+
+        #cleaning the doc
+        a=re.search(r'(.*?)<in-bse-fin:scripcode',doc).end()-21
+        cleandoc=doc[a:]
+        cleandoc=cleandoc.replace("\n", "")
+        print('Doc cleaned')
+
+        #getting contextref
+        cntxt=re.compile(r'contextref=\"(.*?)\"')
+        j=cntxt.findall(cleandoc)
+        df['Context']=j
+
+        #getting the XML tag
+        arg=re.compile(r'</in-bse-fin:(.*?)>')
+        k=arg.findall(cleandoc)
+        df['keys']=k
+
+        #getting the value
+        value=re.compile(r'\">(.*?)</in-bse-fin:',re.DOTALL)
+        n=value.findall(str(cleandoc))
+        df['values']=n
+
+        #sending the dataframe to csv
+        df.set_index('keys',inplace=True)
+        df.to_csv(path+str(self.scripcode)+'.csv')
+        print('Done')
+
+sc=input('Enter Scrip code:')
+company=financials(sc)
+company.bse_xmlparser()
--- Scraper project/Link crawler.py
+++
@@ -1,7 +0,0 @@
-
-import re
-
-def link_crawler(url_start,link_regex):
-    """Crawl from the given start URL following links matched by the link_regex"""
-    crawl_queue=[url_start]
-    while crawl_queue:
